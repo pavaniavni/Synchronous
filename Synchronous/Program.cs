40ea0dba1cd2b128f4827d8c2615e82cc1df4f80
pavani <pavaniavni456@gmail.com>
2022-02-10 11:37:13 +0530

    added synchronous

---
+++ Synchronous/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Work1();
+            Console.WriteLine("..........................");
+            Work2();
+            Console.ReadLine();
+        }
+        static void Work1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work1 is called " + i.ToString());
+            }
+
+        }
+        static void Work2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Work2 is called " + i.ToString());
+            }
+        }
+    }
+}
+
